6c4ee5d2052f7aaf7892e8ab790c0202e9b196c7
Georgiy Korneev <kgeorgiy@keorgiy.info>
2017-03-20 16:34:59 +0300

    Additional tests for Homework 5

--- java/expression/ExpressionTest.java
+++ java/expression/ExpressionTest.java
@@ -17,18 +17,29 @@ public class ExpressionTest {
         testExpression(
                 "x*x+(x-1)/10",
                 new Add(
-                    new Multiply(new Variable("x"), new Variable("x")),
-                    new Divide(new Subtract(new Variable("x"), new Const(1)), new Const(10))
+                        new Multiply(new Variable("x"), new Variable("x")),
+                        new Divide(new Subtract(new Variable("x"), new Const(1)), new Const(10))
                 ),
                 x -> x * x + (x - 1) / 10
         );
+        testExpression("x*-1_000_000_000", new Multiply(new Variable("x"), new Const(-1_000_000_000)), x -> x * -1_000_000_000);
+        testExpression("10/x", new Divide(new Const(10), new Variable("x")), x -> 10 / x);
+        testExpression("x/x", new Divide(new Variable("x"), new Variable("x")), x -> x / x);
         System.out.println("OK");
     }
 
     private static void testExpression(final String description, final Expression actual, final Expression expected) {
         System.out.println("Testing " + description);
         for (int i = 0; i < 10; i++) {
-            assertEquals(String.format("f(%d)", i), actual.evaluate(i), expected.evaluate(i));
+            assertEquals(String.format("f(%d)", i), evaluate(actual, i), evaluate(expected, i));
+        }
+    }
+
+    private static Integer evaluate(final Expression expression, final int x) {
+        try {
+            return expression.evaluate(x);
+        } catch (final ArithmeticException e) {
+            return null;
         }
     }
 }
